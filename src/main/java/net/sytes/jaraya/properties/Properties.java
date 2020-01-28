ab55e7eda0fe62742bc4229bba0806ffb95b56bb
Julián Araya <julian.e.araya81@gmail.com>
2020-01-27 22:53:14 -0300

    act

--- src/main/java/net/sytes/jaraya/properties/Properties.java
+++ src/main/java/net/sytes/jaraya/properties/Properties.java
@@ -5,6 +5,8 @@ import net.sytes.jaraya.exception.UtilException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @Builder
 public class Properties {
@@ -24,7 +26,13 @@ public class Properties {
     public String getProperty(String key) throws UtilException {
         java.util.Properties properties = new java.util.Properties();
         try {
-            properties.load(new FileInputStream(filepath != null && !filepath.isEmpty() ? filepath : DEFAULT_FILEPATH));
+            properties.load(
+                    new InputStreamReader(
+                            new FileInputStream(
+                                    filepath != null && !filepath.isEmpty() ? filepath : DEFAULT_FILEPATH
+                            ), StandardCharsets.UTF_8
+                    )
+            );
         } catch (IOException e) {
             throw new UtilException(e);
         }
